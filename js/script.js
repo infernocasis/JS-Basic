3e4c94139a5b14500c383a8bd22e9d6c28052b7c
Matvey <0990106121sA@mail.ru>
2026-08-31 16:33:20 +0300

    variables

---
+++ js/script.js
@@ -0,0 +1,26 @@
+"use strict"; // мы работает в современном режиме, где не работают некоторые неточности, которые были в старых стандартах js
+
+let number = 5; //= - знак присваивания
+const leftBorderWidth = 1;
+//let это изменияемое значение. const это значение которое мы задали один раз.
+
+// number = 10;
+// console.log(number);
+
+const obj = {
+  a: 50,
+};
+
+obj.a = 10;
+
+console.log(obj);
+
+console.log(name);
+var name = "Ivan";
+//var не нужно использовать в данный момент. из-за хоистинга.
+
+{
+  var result = 50;
+}
+
+console.log(result);
